fc5a0d3fe36276924a6d065248aeab533e8977ee
Lakshit Wasan <lakshitwasan31@gmail.com>
2024-06-18 19:30:55 +0530

    1 added to the number of LL Done right

--- Linked List/add1ToTheNumberRepresentedByLL.cpp
+++ Linked List/add1ToTheNumberRepresentedByLL.cpp
@@ -47,31 +47,126 @@ void print(Node *head)
     cout << endl;
 }
 
-Node *adding1ToRepresentingLLNumber(Node *head)
+// Node *adding1ToRepresentingLLNumber(Node *head)
+// {
+//     Node *temp = head;
+//     int num = 0;
+
+//     while (temp->next)
+//     {
+//         num = (num * 10) + temp->data;
+//         temp = temp->next;
+//     }
+//     Node *newNode = new Node(num + 1);
+//     temp->next = newNode;
+//     return newNode;
+// }
+
+Node *reverseLL(Node *head)
 {
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
     Node *temp = head;
-    int num = 0;
+    Node *prev = nullptr;
 
     while (temp)
     {
-        num = (num * 10) + temp->data;
+        Node *front = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = front;
+    }
+
+    return prev;
+}
+
+Node *adding1ToRepresentingLLNumber_Reversing(Node *head)
+{
+
+    //  TIME COMPELEXITY: O(3N) & SPACE COMPLEXITY: 0(1)
+
+    head = reverseLL(head);
+    Node *temp = head;
+    int carry = 1;
+
+    while (temp)
+    {
+        temp->data = temp->data + carry;
+        if (temp->data < 10)
+        {
+            carry = 0;
+            break;
+        }
+        else
+        {
+            temp->data = 0;
+            carry = 1;
+        }
         temp = temp->next;
     }
-    Node *newNode = new Node(num + 1);
-    return newNode;
+
+    if (carry == 1)
+    {
+        Node *newNode = new Node(carry);
+        head = reverseLL(head);
+        newNode->next = head;
+        return newNode;
+    }
+
+    head = reverseLL(head);
+    return head;
+}
+
+int helper(Node *temp)
+{
+
+    // TIME COMPLEXITY : O(N) * SPACE COMPLEXITY: 0(N)[INT CARRY]
+
+    if (temp == NULL)
+    {
+        return 1;
+    }
+
+    int carry = helper(temp->next);
+
+    temp->data = temp->data + carry;
+    if (temp->data < 10)
+    {
+        return 0;
+    }
+    temp->data = 0;
+    return 1;
+}
+
+Node *adding1ToRepresentingLLNumber_Recursion(Node *head)
+{
+    Node *temp = head;
+    int carry = helper(temp);
+
+    if (carry == 1)
+    {
+        Node *newNode = new Node(1);
+        newNode->next = head;
+        return newNode;
+    }
+    return head;
 }
 
 int main()
 {
-    vector<int> arr = {22, 3, 4, 5};
+    vector<int> arr = {9, 9, 9, 9};
 
     Node *head = arr2LL(arr);
 
     print(head);
 
-    Node *ans = adding1ToRepresentingLLNumber(head);
+    // head = adding1ToRepresentingLLNumber_Reversing(head);
+    head = adding1ToRepresentingLLNumber_Recursion(head);
 
-    cout << ans->data;
+    print(head);
 
     return 0;
 }
