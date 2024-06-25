22f002600b6c7061651ce1eac20b3508aecbd60e
Lakshit Wasan <lakshitwasan31@gmail.com>
2024-06-25 18:06:49 +0530

    33 Question Done

---
+++ Binary Search/33_searchInRotatedSortedArray1.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int> arr, int target)
+{
+    int n = arr.size();
+
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+
+        if (arr[low] <= arr[mid])
+        {
+            if (arr[low] <= target && target <= arr[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (arr[mid] <= target && target <= arr[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    vector<int> arr = {1, 3};
+
+    int ans = search(arr, 3);
+
+    cout << ans;
+
+    return 0;
+}
