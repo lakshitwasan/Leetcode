a20d1b3e5aa0df0dc938fa856b291778050ab42c
Lakshit Wasan <lakshitwasan31@gmail.com>
2024-06-29 11:22:01 +0530

    1539 Question Done

---
+++ Binary Search/1539_kthMissingPositiveNumber.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int kthMissingNumber_Linear(vector<int> arr, int k)
+{
+    // TIME COMPECITY: O(N)
+
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= k)
+        {
+            k++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return k;
+}
+
+int kthMissingNumber_BS(vector<int> arr, int k)
+{
+    int n = arr.size();
+
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int missing = arr[mid] - (mid + 1);
+        if (missing < k)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return low + k;
+}
+
+int main()
+{
+
+    vector<int> arr = {2, 3, 4, 7, 11};
+    int k = 5;
+
+    // int ans = kthMissingNumber_Linear(arr, k);
+    int ans = kthMissingNumber_BS(arr, k);
+    cout << ans;
+
+    return 0;
+}
