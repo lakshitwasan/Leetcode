768d93f39d42adc47981b854dee337eb6077ddb4
Lakshit Wasan <lakshitwasan31@gmail.com>
2024-07-04 23:23:08 +0530

    Postfix to Infix Question Done

---
+++ Stack and Queue/Fix conversions/postfixToInfix.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string postfixToInfix(string s)
+{
+    stack<string> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        char c = s[i];
+
+        if (isalpha(c))
+        {
+            string s2(1, c);
+            st.push(s2);
+        }
+        else if (!st.empty())
+        {
+            string right = st.top();
+            st.pop();
+            string left = st.top();
+            st.pop();
+            // string temp = '(' + left + c + right + ')';
+            // st.push(temp);
+            // st.push('(' + left + c + right + ')'); THE ABOVE CODE IS GIVING TLE BECAUSE A NEW STRING IS BEING CREATED JUST TO PUSH
+        }
+    }
+    return st.top();
+}
+
+int main()
+{
+    string exp = "ab+ef/*";
+    cout << "Postfix expression: " << exp << endl;
+    cout << "Infix expression: " << postfixToInfix(exp) << endl;
+    return 0;
+    return 0;
+}
