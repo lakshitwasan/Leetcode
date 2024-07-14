8fbc7f0181dcef245cc944a0c753a68dbd10eaf0
Lakshit Wasan <lakshitwasan31@gmail.com>
2024-07-15 00:22:39 +0530

    1089 duplicate zeroes question done

---
+++ Array/1089_duplicateZeroes.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void duplicateZeroes(vector<int> &arr)
+{
+    int i = 0;
+    int n = arr.size();
+    while (i < n)
+    {
+        if (arr[i] == 0)
+        {
+            arr.insert(arr.begin() + i, 0);
+            i = i + 2;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    arr.resize(n);
+}
+
+int main()
+{
+
+    vector<int> arr = {1, 0, 2, 3, 0, 4, 5, 0};
+
+    duplicateZeroes(arr);
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
