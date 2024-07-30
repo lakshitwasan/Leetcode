29ba532025223336bba037318df24137ae103787
Lakshit Wasan <lakshitwasan31@gmail.com>
2024-07-31 03:32:04 +0530

    # question done with slidig window approach

--- Sliding Window/3_longestSubstringWithoutRepeatingCharacters.cpp
+++ Sliding Window/3_longestSubstringWithoutRepeatingCharacters.cpp
@@ -1,78 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lengthOfLongestSubstring(string s)
+int lengthOfLongestSubstring_bruteForce(string s)
 {
+
+    // TIME COMPLEXITY: O(N^2) & SPACE COMPLEXITY: O(N)
+
     int n = s.size();
 
+    if (n == 0)
+    {
+        return 0;
+    }
+
     int maxLength = 0;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     unordered_set<int> set;
-
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         if (set.find(s[j]) != set.end())
-    //         {
-    //             break;
-    //         }
-    //         int len = j - i + 1;
-    //         maxLength = max(maxLength, len);
-    //         set.insert(s[j]);
-    //     }
-    // }
-    // return maxLength;
-
-    for (int i = 0; i < s.length(); i++)
+    for (int i = 0; i < n; i++)
     {
         unordered_set<int> set;
-        for (int j = i; j < s.length(); j++)
+
+        for (int j = i; j < n; j++)
         {
             if (set.find(s[j]) != set.end())
             {
-                maxLength = max(maxLength, j - i);
                 break;
             }
+            int len = j - i + 1;
+            maxLength = max(maxLength, len);
             set.insert(s[j]);
         }
     }
     return maxLength;
 }
 
-class Solution
+int lengthOfLongestSubstring_slidingWindow(string s)
 {
-public:
-    int lengthOfLongestSubstring(string s)
-    {
-        int n = s.length();
-        int maxLength = 0;
-        unordered_set<char> charSet;
-        int left = 0;
 
-        for (int right = 0; right < n; right++)
+    // TIME COMPLEXITY: O(N) & SPACE COMPLEXITY: O(N)
+
+    int n = s.size();
+
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    vector<int> hash(128, -1);
+
+    int left = 0;
+    int right = 0;
+    int maxlength = 0;
+
+    while (right < n)
+    {
+        if (hash[s[right]] != -1)
         {
-            if (charSet.count(s[right]) == 0)
+            if (hash[s[right]] >= left)
             {
-                charSet.insert(s[right]);
-                maxLength = max(maxLength, right - left + 1);
-            }
-            else
-            {
-                while (charSet.count(s[right]))
-                {
-                    charSet.erase(s[left]);
-                    left++;
-                }
-                charSet.insert(s[right]);
+                left = hash[s[right]] + 1;
             }
         }
-
-        return maxLength;
+        int len = right - left + 1;
+        maxlength = max(len, maxlength);
+        hash[s[right]] = right;
+        right++;
     }
-};
+
+    return maxlength;
+}
 
 int main()
 {
+    string s = "lakshitwasan";
+
+    int ans1 = lengthOfLongestSubstring_bruteForce(s);
+    int ans2 = lengthOfLongestSubstring_slidingWindow(s);
+
+    cout << "Ans1: " << ans1 << endl;
+    cout << "Ans2: " << ans2 << endl;
 
     return 0;
 }
