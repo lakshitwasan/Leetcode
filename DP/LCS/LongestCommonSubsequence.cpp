4e7ca17b3d00dff2c994445eca4d06febe99bd0b
Lakshit Wasan <95538553+lakshitwasan@users.noreply.github.com>
2024-09-09 23:11:54 +0530

    LCS Done using top down (DP)

--- DP/LCS/LongestCommonSubsequence.cpp
+++ DP/LCS/LongestCommonSubsequence.cpp
@@ -45,6 +45,42 @@ int LCS_memoized(vector<char> x, vector<char> y, int n, int m)
     }
 }
 
+vector<vector<int>> dp2;
+
+int LCS_TopDown(vector<char> x, vector<char> y, int n, int m)
+{
+    vector<vector<int>> dp2(n + 1, vector<int>(m + 1));
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                dp2[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+
+            if (x[i - 1] == y[j - 1])
+            {
+                dp2[i][j] = 1 + dp2[i - 1][j - 1];
+            }
+            else
+            {
+                dp2[i][j] = max(dp2[i - 1][j], dp2[i][j - 1]);
+            }
+        }
+    }
+
+    return dp2[n][m];
+}
+
 int main()
 {
     vector<char> x = {'A', 'B', 'C', 'D', 'G', 'H'};
@@ -57,9 +93,11 @@ int main()
     cout << "Length of Longest Common Subsequence (Recursive): " << lengthLCS_recursive << endl;
 
     dp = vector<vector<int>>(n + 1, vector<int>(m + 1, -1));
-
     int lengthLCS_memoized = LCS_memoized(x, y, n, m);
     cout << "Length of Longest Common Subsequence (Memoized): " << lengthLCS_memoized << endl;
+
+    int lengthLCS_topdown = LCS_TopDown(x, y, n, m);
+    cout << "Length of Longest Common Subsequence (Top-Down): " << lengthLCS_topdown << endl;
 
     return 0;
 }
