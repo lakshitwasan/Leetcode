23d55705619493773c2b0776a161808531f03d33
lakshitwasan <lakshitwasan31@gmail.com>
2025-08-26 00:53:18 +0530

    Level Order traversal done

---
+++ Tree/102_BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+vector<vector<int>> levelorder(Node *root)
+{
+    vector<vector<int>> ans;
+    if (root == NULL)
+    {
+        return ans;
+    }
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        vector<int> level;
+        int size = q.size();
+        for (int i = 0; i < size; i++)
+        {
+            Node *node = q.front();
+            q.pop();
+            if (node->left != nullptr)
+            {
+                q.push(node->left);
+            }
+            if (node->right != nullptr)
+            {
+                q.push(node->right);
+            }
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+
+    return ans;
+}
+
+int main()
+{
+    Node *root = new Node(4);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->left->left = new Node(3);
+    root->left->left->right = new Node(9);
+    root->left->left->right->left = new Node(1);
+    root->right->left = new Node(7);
+    root->right->right = new Node(6);
+    root->right->right->left = new Node(8);
+
+    vector<vector<int>> result = levelorder(root);
+
+    cout << "Level Order Traversal: \n";
+    for (auto &level : result)
+    {
+        for (auto &val : level)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
