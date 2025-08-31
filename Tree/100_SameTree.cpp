e804c0eec957315df7739eacbc454248b6bb0918
lakshitwasan <lakshitwasan31@gmail.com>
2025-08-31 22:52:10 +0530

    Are two trees identical done

---
+++ Tree/100_SameTree.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) : data(val), left(NULL), right(NULL) {}
+};
+
+bool sameTree(Node *root1, Node *root2)
+{
+    if (root1 == NULL && root2 == NULL)
+        return true;
+    if (root1 == NULL || root2 == NULL)
+        return false;
+    return (root1->data == root2->data) && sameTree(root1->left, root2->left) && sameTree(root1->right, root2->right);
+}
+
+int main()
+{
+    Node *root1 = new Node(1);
+    root1->left = new Node(2);
+    root1->right = new Node(3);
+    root1->left->left = new Node(4);
+    root1->left->right = new Node(5);
+
+    Node *root2 = new Node(1);
+    root2->left = new Node(2);
+    root2->right = new Node(3);
+    root2->left->left = new Node(4);
+    root2->left->right = new Node(5);
+
+    if (sameTree(root1, root2))
+        cout << "Same" << endl;
+    else
+        cout << "Not Same" << endl;
+    return 0;
+}
