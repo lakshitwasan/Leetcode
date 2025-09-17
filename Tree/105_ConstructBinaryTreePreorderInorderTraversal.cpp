f1010ea9495c6cbf1d895c7a94f0bed22c976e02
lakshitwasan <lakshitwasan31@gmail.com>
2025-09-17 19:32:32 +0530

    BT using inorder and preorder done

---
+++ Tree/105_ConstructBinaryTreePreorderInorderTraversal.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+Node *BuildTreeHelper(vector<int> preorder, int preStart, int preEnd, vector<int> inorder, int inStart, int inEnd, map<int, int> &inMap)
+{
+    if (preStart > preEnd || inStart > inEnd)
+    {
+        return NULL;
+    }
+
+    Node *root = new Node(preorder[preStart]);
+
+    int inRoot = inMap[root->data];
+    int numsLeft = inRoot - inStart;
+
+    root->left = BuildTreeHelper(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, inMap);
+    root->right = BuildTreeHelper(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, inMap);
+
+    return root;
+}
+
+Node *BuildTree(vector<int> preorder, vector<int> inorder)
+{
+    map<int, int> inMap;
+    for (int i = 0; i < inorder.size(); i++)
+    {
+        inMap[inorder[i]] = i;
+    }
+
+    Node *root = BuildTreeHelper(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, inMap);
+
+    return root;
+}
+
+
+void printInorder(Node *root)
+{
+    if (!root)
+        return;
+    printInorder(root->left);
+    cout << root->data << " ";
+    printInorder(root->right);
+}
+
+void printPreorder(Node *root)
+{
+    if (!root)
+        return;
+    cout << root->data << " ";
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
+
+int main()
+{
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+
+    Node *root = BuildTree(preorder, inorder);
+
+    cout << "Inorder traversal of constructed tree: ";
+    printInorder(root);
+    cout << endl;
+
+    cout << "Preorder traversal of constructed tree: ";
+    printPreorder(root);
+    cout << endl;
+
+    return 0;
+}
