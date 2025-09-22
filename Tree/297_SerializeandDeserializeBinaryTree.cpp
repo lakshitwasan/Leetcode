7dc3780e15fc5458793d9de0779e333dbe6b95a6
lakshitwasan <lakshitwasan31@gmail.com>
2025-09-22 23:46:08 +0530

    Serialize and deserialize of Binary Tree done

---
+++ Tree/297_SerializeandDeserializeBinaryTree.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+string serialize(Node *root)
+{
+    if (!root)
+    {
+        return "";
+    }
+    string s = "";
+
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+        if (!node)
+        {
+            s += "#,";
+        }
+        else
+        {
+            s += to_string(node->data) + ",";
+            q.push(node->left);
+            q.push(node->right);
+        }
+    }
+
+    return s;
+}
+
+Node *deserialize(string data)
+{
+    if (data.empty())
+    {
+        return NULL;
+    }
+    stringstream s(data);
+    string str;
+
+    getline(s, str, ',');
+    Node *root = new Node(stoi(str));
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        q.pop();
+
+        getline(s, str, ',');
+
+        if (str != "#")
+        {
+            Node *leftNode = new Node(stoi(str));
+            node->left = leftNode;
+            q.push(leftNode);
+        }
+
+        getline(s, str, ',');
+
+        if (str != "#")
+        {
+            Node *rightNode = new Node(stoi(str));
+            node->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+
+    return root;
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->right->left = new Node(4);
+    root->right->right = new Node(5);
+
+    string data = serialize(root);
+    cout << "Serialized tree: " << data << endl;
+
+    Node *newRoot = deserialize(data);
+
+    string check = serialize(newRoot);
+    cout << "Serialized after deserialization: " << check << endl;
+
+    return 0;
+}
