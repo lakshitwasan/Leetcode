4328c40ab901a8831be61fdfd09ebfe7195d6089
lakshitwasan <lakshitwasan31@gmail.com>
2025-09-26 22:14:52 +0530

    Preorder using morris method done

--- Tree/144_BinaryTreePreorderTraversal.cpp
+++ Tree/144_BinaryTreePreorderTraversal.cpp
@@ -67,6 +67,43 @@ vector<int> preOrder_stack(Node *root)
     return ans;
 }
 
+vector<int> preorder_Morris(Node *root)
+{
+
+    // TIME COMPLEXITY: O(N) & SPACE COMPLEXITY: O(1) 
+
+    vector<int> preorder;
+    Node *cur = root;
+    while (cur != NULL)
+    {
+        if (cur->left == NULL)
+        {
+            preorder.push_back(cur->data);
+            cur = cur->right;
+        }
+        else
+        {
+            Node *prev = cur->left;
+            while (prev->right && prev->right != cur)
+            {
+                prev = prev->right;
+            }
+            if (prev->right == NULL)
+            {
+                prev->right = cur;
+                preorder.push_back(cur->data);
+                cur = cur->left;
+            }
+            else
+            {
+                prev->right = NULL;
+                cur = cur->right;
+            }
+        }
+    }
+    return preorder;
+}
+
 int main()
 {
     Node *root = new Node(4);
