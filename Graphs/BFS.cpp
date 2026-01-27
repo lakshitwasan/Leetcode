c7d3d1fa813ee7239ab3bcb89b5dbeb9505a2056
lakshitwasan <lakshitwasan31@gmail.com>
2026-01-27 20:44:23 +0530

    BFS Graph done

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> BFS(int V, vector<int> adj[])
+{
+    int vis[V] = {0};
+    vis[0] =1;
+    queue<int> q;
+    q.push(0);
+    vector<int>bfs;
+    while(!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+
+        for(auto it: adj[node])
+        {
+            if (!vis[it])
+            {
+                vis[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+
+    return bfs;
+}
+
+int main()
+{
+    int V = 7;
+    vector<int> adj[V];
+
+    adj[0].push_back(1);
+    adj[1].push_back(0);
+
+    adj[0].push_back(2);
+    adj[2].push_back(0);
+
+    adj[1].push_back(3);
+    adj[3].push_back(1);
+
+    adj[1].push_back(4);
+    adj[4].push_back(1);
+
+    adj[2].push_back(5);
+    adj[5].push_back(2);
+
+    adj[4].push_back(6);
+    adj[6].push_back(4);
+
+    vector<int> bfs = BFS(V, adj);
+
+    cout << "BFS Traversal: ";
+    for (int node : bfs)
+        cout << node << " ";
+
+    return 0;
+}
