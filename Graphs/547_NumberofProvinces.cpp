e797ac5e22c461738d260ed469b2e7721d76cfe2
lakshitwasan <lakshitwasan31@gmail.com>
2026-01-28 01:30:04 +0530

    547 No of provinces done

---
+++ Graphs/547_NumberofProvinces.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs(int node, vector<int> adj[], vector<int> &vis)
+{
+    vis[node] = 1;
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+        {
+            dfs(it, adj, vis);
+        }
+    }
+}
+
+int NoOfProvince(vector<vector<int>> &isConnected)
+{
+    int V = isConnected.size();
+    vector<int> adj[V];
+
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            if (isConnected[i][j] == 1 && i != j)
+            {
+                adj[i].push_back(j);
+            }
+        }
+    }
+
+    vector<int> vis(V, 0);
+    int count = 0;
+
+    for (int i = 0; i < V; i++)
+    {
+        if (!vis[i])
+        {
+            count++;
+            dfs(i, adj, vis);
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    vector<vector<int>> isConnected = {
+        {1, 1, 0},
+        {1, 1, 0},
+        {0, 0, 1}};
+
+    cout << NoOfProvince(isConnected);
+    return 0;
+}
